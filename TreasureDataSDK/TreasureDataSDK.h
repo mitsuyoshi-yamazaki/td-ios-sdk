4fde84d1bb848678b01a048c49156e11b73ad92e
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-04-14 15:42:14 +0900

    Create Cocoa Touch Framework

---
+++ TreasureDataSDK/TreasureDataSDK.h
@@ -0,0 +1,21 @@
+//
+//  TreasureDataSDK.h
+//  TreasureDataSDK
+//
+//  Created by mitsuyoshi.yamazaki on 2017/04/14.
+//  Copyright © 2017年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TreasureData.h"
+
+
+//! Project version number for TreasureDataSDK.
+FOUNDATION_EXPORT double TreasureDataSDKVersionNumber;
+
+//! Project version string for TreasureDataSDK.
+FOUNDATION_EXPORT const unsigned char TreasureDataSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreasureDataSDK/PublicHeader.h>
+
+
